6027bf8adbf59758c771b986013560dfe5d274b7
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-10-01 09:43:43 -0300

    fix logic about removing expired item

--- src/get-data/app.js
+++ src/get-data/app.js
@@ -1,18 +1,16 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const EXPIRATION_TIME = parseInt(process.env.EXPIRATION_TIME); // 1 hora
+const EXPIRATION_TIME = parseInt(process.env.EXPIRATION_TIME);
 
 exports.handler = async (event) => {
     const slug = event.pathParameters.slug;
     const currentTimestamp = Date.now();
 
-    // Busca o item no DynamoDB
     const result = await dynamo.get({
         TableName: process.env.TABLE_NAME,
         Key: { id: slug },
     }).promise();
 
-    // Verifica se o item existe
     if (!result.Item) {
         return {
             statusCode: 404,
@@ -20,12 +18,10 @@ exports.handler = async (event) => {
         };
     }
 
-    // Extrai o timestamp do Snowflake ID e calcula a expiração
-    const snowflakeTimestamp = parseInt(slug.slice(0, 10), 16); // Os primeiros 41 bits do ID
-    const creationTime = snowflakeTimestamp + 1288834974657; // Ajusta o timestamp para o Epoch
+    const snowflakeTimestamp = BigInt(slug) >> 22n;
+    const creationTime = Number(snowflakeTimestamp) + 1288834974657;
 
     if (currentTimestamp - creationTime > EXPIRATION_TIME * 1000) {
-        // Se já expirou, apaga o item e retorna um erro
         await dynamo.delete({
             TableName: process.env.TABLE_NAME,
             Key: { id: slug },
@@ -37,7 +33,6 @@ exports.handler = async (event) => {
         };
     }
 
-    // Se ainda não expirou, retorna os dados
     return {
         statusCode: 200,
         body: JSON.stringify(result.Item.data),
